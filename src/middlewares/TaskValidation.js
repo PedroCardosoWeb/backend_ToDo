1b9e001e4453a15d95b253a6354e40f597d0a797
PedroCastilho <pedrocastilhoc@outlook.com>
2021-09-13 20:55:43 -0300

    add config, controllers, middlewares, models

---
+++ src/middlewares/TaskValidation.js
@@ -0,0 +1,41 @@
+const TaskModel = require('../model/TaskModel');
+const { isPast } = require ('date-fns');
+
+const TaskValidation = async (req, res, next) => {
+    const {macaddress, type, title, description, when} = req.body;
+    if(!macaddress)
+        return res.status(400).json({ error: 'MacAddress é obrigatório!'});
+        else if (!type)
+            return res.status(400).json({ error: 'Tipo é obrigatório!'});
+            else if (!title)
+                return res.status(400).json({ error: 'Título é obrigatório!'});
+                else if (!description)
+                    return res.status(400).json({ error: 'Descrição é obrigatória!'});
+                    else if (!when)
+                        return res.status(400).json({ error: 'Data e Hora são obrigatórios!'});
+                        else if (isPast(new Date(when)))
+                            return res.status(400).json({ error: 'Não é possível agendar um compromisso no passado :( '})
+                            else{
+                                let exists;
+
+                                if(req.params.id){
+                                    exists = await TaskModel.findOne({
+                                        '_id': {'$ne': req.params.id},
+                                        'when': {'$eq': new Date(when)},
+                                        'macaddress': {'$in': macaddress}
+                                    });
+                                } else {
+                                    exists = await TaskModel.findOne({
+                                        'when': {'$eq': new Date(when)},
+                                        'macaddress': {'$in': macaddress}
+                                    });
+                                }
+                               
+                                if(exists){
+                                    return res.status(400).json({ error: 'Já existe um compromisso nesta data!'})
+                                }
+                                next();
+                            }
+}
+
+module.exports =  TaskValidation;
---
+++ src/routes/TaskRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controller/TaskController');
+const TaskValidation = require('../middlewares/TaskValidation');
+
+router.post('/', TaskValidation, TaskController.create);
+router.put('/:id', TaskValidation, TaskController.update);
+router.get('/filter/all', TaskController.all)
+
+module.exports = router;
+    
