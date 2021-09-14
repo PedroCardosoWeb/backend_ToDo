0be563e99ecb2be5725509d9eff226d6dea40989
PedroCastilho <pedrocastilhoc@outlook.com>
2021-09-13 23:05:19 -0300

    add show/ delete task

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -34,6 +34,29 @@ class TaskController {
     })
     }
 
+    async show (req, res){
+        await TaskModel.findById(req.params.id)
+            .then(response => {
+                if(response)
+                    return res.status(200).json(response);
+                else
+                    return res.status(404).json({error: 'Tarefa não encontrada!'});
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async delete (req, res){
+        await TaskModel.deleteOne({'_id': req.params.id})
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
 }
 
 module.exports = new TaskController();
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
-router.get('/filter/all', TaskController.all)
+router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
+router.get('/filter/all', MacaddressValidation, TaskController.all)
 
 module.exports = router;
     
