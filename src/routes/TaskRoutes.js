89d19b01b230bcdf09fc9e9e8c9addfd6a3d015e
PedroCastilho <pedrocastilhoc@outlook.com>
2021-09-14 18:17:18 -0300

    altera status/ exibe atrasados

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -7,9 +7,11 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
+router.put('/:id/:done', TaskController.done);
 router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
-router.get('/filter/all', MacaddressValidation, TaskController.all)
+router.get('/filter/all', MacaddressValidation, TaskController.all);
+router.get('/filter/late', MacaddressValidation, TaskController.late);
 
 module.exports = router;
     
