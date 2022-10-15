27145f04b6fa4c640d6e5266cc08244d6faf9b5d
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2022-10-15 17:10:50 -0300

    test env var

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = MONGO_URL
-// const url = 'mongodb://localhost:27017/todo';
+const url = 'mongodb://localhost:27017/todo';
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(url, { useNewUrlParser : true });
 
